ff1cd2fce7043b2b3e6f11a3738693513ca3dce6
Paul Asitik <paasitik@gmail.com>
2023-10-23 15:38:03 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-base = "https://jsonplaceholder.typicode.com"
-id = sys.argv[1]
-users = "/users/{}".format(id)
-todos = "/todos?userId={}".format(id)
 
-rEmp = requests.get(base + users)
-rTodos = requests.get(base + todos)
-tasks = rTodos.json()
-EMPLOYEE_NAME = rEmp.json()["name"]
-completed_tasks = 0
-for task in tasks:
-    if task["completed"] is True:
-        completed_tasks += 1
-print("Employee {} is done with tasks({}/{})"
+if __name__ == "__main__":
+    base = "https://jsonplaceholder.typicode.com"
+    id = sys.argv[1]
+    users = "/users/{}".format(id)
+    todos = "/todos?userId={}".format(id)
+
+    rEmp = requests.get(base + users)
+    rTodos = requests.get(base + todos)
+    tasks = rTodos.json()
+    EMPLOYEE_NAME = rEmp.json()["name"]
+    completed_tasks = 0
+    for task in tasks:
+        if task["completed"] is True:
+            completed_tasks += 1
+    print("Employee {} is done with tasks({}/{})"
       .format(EMPLOYEE_NAME, completed_tasks, len(tasks)))
-for task in tasks:
-    if task["completed"] == True:
-        print("\t {}\n".format(task["title"]))
+    for task in tasks:
+        if task["completed"] == True:
+            print("\t {}\n".format(task["title"]))
