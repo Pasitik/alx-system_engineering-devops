6410e543c3b71778d62409b1a8f061bf90207a26
Paul Asitik <paasitik@gmail.com>
2023-10-23 15:10:57 -0700

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+base = "https://jsonplaceholder.typicode.com"
+id = sys.argv[1]
+users = "/users/{}".format(id)
+todos = "/todos?userId={}".format(id)
+
+rEmp = requests.get(base + users)
+rTodos = requests.get(base + todos)
+tasks = rTodos.json()
+EMPLOYEE_NAME = rEmp.json()["name"]
+completed_tasks = 0
+for task in tasks:
+    if task["completed"] == True:
+        completed_tasks += 1
+print("Employee {} is done with tasks({}/{})".format(EMPLOYEE_NAME, completed_tasks, len(tasks)))
+
+for task in tasks:
+    if task["completed"] == True:
+        print("\t {}\n".format(task["title"]))
+
+
