6dcc3581fda877dbe0804b3b14ffab102c52d681
pasitik <paasitik@gmail.com>
2023-11-07 11:12:15 -0800

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import requests
+
+def number_of_subscribers(subreddit):
+    """get subscribers of a given subreddit """
+
+    if subreddit is None:
+        return 0
+
+    url = f"https://www.reddit.com/r/{subreddit}/about/.json"
+    r = requests.get(url)
+
+    if r.status_code != 200:
+        return 0
+
+    data = r.json()["data"]["subscribers"]
+    return (data)
+
